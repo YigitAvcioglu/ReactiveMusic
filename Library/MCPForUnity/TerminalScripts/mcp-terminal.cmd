35bccd30673aeb088256e49f47de664b5e51e3b5
YigitAvcioglu <avcioglu.yigitt@gmail.com>
2026-04-02 20:42:20 +0300

    first commit
    created vr enviroment

---
+++ Library/MCPForUnity/TerminalScripts/mcp-terminal.cmd
@@ -0,0 +1,3 @@
+@echo off
+cls
+C:\Users\avcio\.local\bin\uvx.exe --offline --prerelease explicit --from "mcpforunityserver>=0.0.0a0" mcp-for-unity --transport http --http-url http://127.0.0.1:8080 --project-scoped-tools --pidfile C:\Users\avcio\Documents\GitHub\VR_Game\VR_Game\Library\MCPForUnity\RunState\mcp_http_8080.pid --unity-instance-token ed6210d0eb5547dab1de5e9abe299ef6
